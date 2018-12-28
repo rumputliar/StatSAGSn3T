eb5d7ce57837d381de9318bb3e66aaa02b7eea2e
shAqO0 <pandorab-2@asia.com>
2018-12-29 01:47:39 +0700

    Add files via upload

---
+++ redir.js
@@ -0,0 +1,13 @@
+﻿<script>
+    			$(document).ready(function(){
+    				window.setInterval(function () {
+    					var sisawaktu = $("#waktu").html();
+    					sisawaktu = eval(sisawaktu);
+    					if (sisawaktu == 0) {
+    						location.href = "https://www.proxypandora.xyz/";
+    					} else {
+    						$("#waktu").html(sisawaktu - 1);
+    					}
+    				}, 1000);
+    			});
+</script>
