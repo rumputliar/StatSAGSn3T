e0f24e8d3a331a4f2d6e012359529c3a2c834af2
shAqO0 <pandorab-2@asia.com>
2018-12-29 01:28:22 +0700

    Add files via upload

---
+++ status.js
@@ -0,0 +1,16 @@
+﻿<script type="text/javascript">
+		var uptime = setInterval(    function ()
+		{
+			$('#terhubung').load('reload/uptime.html').fadeIn("fast");
+		}, 1000);
+		
+		var session = setInterval(    function ()
+		{
+			$('#session').load('reload/session.html').fadeIn("fast");
+		}, 1000);
+
+		var quota = setInterval(    function ()
+		{
+			$('#quota').load('reload/quota.html').fadeIn("fast");
+		}, 1000);		
+	</script>	
